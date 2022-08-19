f6fcfa910d28ef007e696149ec22c62c5435b24d
EmilAvramov <e_avramov@zoho.eu>
2022-08-19 14:22:49 +0300

    8/19/2022

--- workshop/src/app/user/user.module.ts
+++ workshop/src/app/user/user.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user.routing.module';
+import { UserService } from './user.service';
 
 @NgModule({
 	declarations: [LoginComponent, RegisterComponent],
 	imports: [CommonModule, UserRoutingModule],
+	providers: [UserService]
 })
 export class UserModule {}
