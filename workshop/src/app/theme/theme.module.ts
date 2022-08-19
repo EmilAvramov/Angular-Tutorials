450c792ab1b36a7e29e8acd8fc4c037168a1b83b
EmilAvramov <e_avramov@zoho.eu>
2022-08-19 15:32:29 +0300

    8/19/2022

--- workshop/src/app/theme/theme.module.ts
+++ workshop/src/app/theme/theme.module.ts
@@ -4,9 +4,16 @@ import { NewThemeComponent } from './new-theme/new-theme.component';
 import { ThemesComponent } from './themes/themes.component';
 import { ThemeRoutingModule } from './theme.routing.module';
 import { ThemeComponent } from './theme/theme.component';
+import { AsideComponent } from './aside/aside.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-	declarations: [NewThemeComponent, ThemesComponent, ThemeComponent],
-	imports: [CommonModule, ThemeRoutingModule],
+	declarations: [
+		NewThemeComponent,
+		ThemesComponent,
+		ThemeComponent,
+		AsideComponent,
+	],
+	imports: [CommonModule, ThemeRoutingModule, SharedModule],
 })
 export class ThemeModule {}
